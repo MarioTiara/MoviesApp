3a503b983ff4d93c9d37502c7063d5c2bf5e1986
MarioTiara <mario.tiarapratama@gmail.com>
2021-08-18 14:10:49 +0700

    Add form to Create/Edit Movie

--- MoviesApp/Controllers/MoviesController.cs
+++ MoviesApp/Controllers/MoviesController.cs
@@ -44,14 +44,9 @@ namespace MoviesApp.Controllers
 		}
 		
 		
-		public ActionResult Details(int id)
-		{
-			var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
-			if (movie == null)
-				return HttpNotFound();
-			return View(movie);
-		}
+		
 
+		[HttpPost]
 		public ActionResult Save(Movie movie)
 		{
 			if (movie.Id==0)
@@ -70,6 +65,30 @@ namespace MoviesApp.Controllers
 			}
 			_context.SaveChanges();	
 			return RedirectToAction("Index", "Movies");
+		}
+
+		public ActionResult Details(int id)
+		{
+			var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
+			if (movie == null)
+				return HttpNotFound();
+			return View(movie);
+		}
+
+		public ActionResult Edit(int id)
+		{
+			var movie = _context.Movies.SingleOrDefault(m => m.Id == id);
+			if (movie == null)
+				return HttpNotFound();
+
+			var viewModel=new MovieFormViewModel
+			{
+			   Movie=movie,
+			   Genres=_context.Genres.ToList()
+			};
+
+			return View("MovieForm", viewModel);
+
 		}
 	}
 }
